1cff47363f5a75ec1c9f38d11f176770f302c17e
vlad-fray <73135622+vlad-fray@users.noreply.github.com>
2020-12-20 13:10:49 +0400

    а

--- #source/js/main.js
+++
@@ -1,1 +0,0 @@
-//@prepros-append-script.js
---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+let makeTargets = function(targets) {
+    for (let i=0; i<targets.length; i++) {
+        const className = targets[i].getAttribute('href').replace('#','.');
+        console.log(className);
+        if (className != '.'){
+            const scrollTarget = document.querySelector(className).
+                    getBoundingClientRect().top;
+            targets[i].addEventListener('click', () => window.scrollTo(
+                {
+                    top: scrollTarget,
+                    behavior: 'smooth',
+                }
+            ))
+        }
+
+    }
+};
+
+let ScrollTargets = document.querySelectorAll('.menu-tab');
+makeTargets(ScrollTargets);
+ScrollTargets = document.querySelectorAll('.footer__nav-tab');
+makeTargets(ScrollTargets);
