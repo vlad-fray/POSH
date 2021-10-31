6d6afaaf84e87fc9a4862bb01a86657d821b0db0
VFray <rekrut313@gmail.com>
2021-10-31 14:16:00 +0300

    Refactoring
    1. Refactoring scss files` structure
    2. Small fix header markup

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,16 @@
-let makeTargets = function(targets) {
-    for (let i=0; i<targets.length; i++) {
-        const className = targets[i].getAttribute('href').replace('#','.');
-        console.log(className);
-        if (className != '.'){
-            const scrollTarget = document.querySelector(className).
-                    getBoundingClientRect().top;
-            targets[i].addEventListener('click', () => window.scrollTo(
-                {
-                    top: scrollTarget,
-                    behavior: 'smooth',
-                }
-            ))
-        }
-
+let makeTargets = function (targets) {
+  for (let i = 0; i < targets.length; i++) {
+    const className = targets[i].getAttribute('href').replace('#', '.');
+    if (className != '.') {
+      const scrollTarget = document.querySelector(className).getBoundingClientRect().top;
+      targets[i].addEventListener('click', () =>
+        window.scrollTo({
+          top: scrollTarget,
+          behavior: 'smooth',
+        })
+      );
     }
+  }
 };
 
 let ScrollTargets = document.querySelectorAll('.menu-tab');
